3821acbe5715350072e598b1bc444f598122c1f4
Guled12345 <g.warsameh@alustudent.com>
2023-07-16 07:10:57 -0700

    2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-""""Doc"""
+"""Module documentation"""
 import requests
 
+headers = {'User-Agent': 'MyAPI/0.0.1'}
 
-def recurse(subreddit, hot_list=[], after=""):
-    """"Doc
-    Reddit sends an after property in the response.
-    Keep retrieving comments until after is null.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-    param = {'after': after}
-    res = requests.get(url, headers=header, params=param)
 
-    if res.status_code != 200:
-        return None
+def recurse(subreddit, after="", hot_list=[], page_count=0):
+
+    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
+
+    parameters = {'limit': 100, 'after': after}
+    response = requests.get(subreddit_url, headers=headers, params=parameters)
+
+    if response.status_code == 200:
+        json_data = response.json()
+
+        for child in json_data.get('data').get('children'):
+            title = child.get('data').get('title')
+            hot_list.append(title)
+
+        after = json_data.get('data').get('after')
+        if after is not None:
+
+            page_count += 1
+            return recurse(subreddit, after=after,
+                           hot_list=hot_list, page_count=page_count)
+        else:
+            return hot_list
+
     else:
-        json_res = res.json()
-        # print(json_res.get('data').get('after'))
-        after = json_res.get('data').get('after')
-        has_next = \
-            json_res.get('data').get('after') is not None
-        # print(has_next)
-        hot_articles = json_res.get('data').get('children')
-        [hot_list.append(article.get('data').get('title'))
-         for article in hot_articles]
-        # print(len(hot_list))
-        # print(hot_list)
-        return recurse(subreddit, hot_list, after=after) \
-            if has_next else hot_list
+        return None
+
+
+if __name__ == '__main__':
+    print(recurse("zerowastecz"))
